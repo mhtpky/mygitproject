6c7647ead8f3d2ab697d00b6669553451fd9d2df
mhtpky <78981082+mhtpky@users.noreply.github.com>
2021-04-04 19:10:17 +0300

    Update JavaGitClass.java

--- src/mygitpackage/JavaGitClass.java
+++ src/mygitpackage/JavaGitClass.java
@@ -1,14 +1,14 @@
 package mygitpackage;
 
 public class JavaGitClass {
-
-	public static void main(String [] args) {
-		
+	public static void main(String [] args) {	
 		System.out.println("Version 1");
 		System.out.println("My second commit");
 		System.out.println("My third commit");
 		System.out.println("My 4th commit");
 		System.out.println("My 5th commit");
-		
+		System.out.println("Uzak sunucuda ki kodlarim");
+		System.out.println("Yeni bir satir");
+		System.out.println("12. satir UZAK SUNUCU");
 	}
 }
